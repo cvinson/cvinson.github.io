9eb7c175093ee1b9fb7ae05fb2d22c4da2a34841
Colin Vinson <cvinson86@gmail.com>
2017-09-19 15:31:04 -0500

    Added contact icons to avatar overlay

--- src/QuickInfo/QuickInfo.js
+++ src/QuickInfo/QuickInfo.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import Mailto from 'react-mailto';
 import Avatar from 'material-ui/Avatar';
 import { Card, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import { List, ListItem } from 'material-ui/List';
+import IconButton from 'material-ui/IconButton';
+import EmailIcon from 'material-ui/svg-icons/communication/email';
+import CodeIcon from 'material-ui/svg-icons/action/code';
+import ResumeIcon from 'material-ui/svg-icons/editor/insert-drive-file';
 import SchoolIcon from 'material-ui/svg-icons/social/school';
 import './QuickInfo.css';
+
+const Overlay = () => (
+  <div className="Overlay">
+    <Mailto email={['colinvinson', 'protonmail.com'].join('@')} obfuscate={true}>
+      <IconButton tooltip="Email Me">
+        <EmailIcon />
+      </IconButton>
+    </Mailto>
+    <a href="https://github.com/cvinson">
+      <IconButton tooltip="Github">
+        <CodeIcon />
+      </IconButton>
+    </a>
+    <a href="https://github.com/cvinson">
+      <IconButton tooltip="Download My Resume">
+        <ResumeIcon />
+      </IconButton>
+    </a>
+  </div>
+);
 
 export default () => {
   return (
     <div className="QuickInfo">
       <Card className="QuickInfoCard">
-        <CardMedia>
-          <img src="http://colinvinson.com/images/me.jpg" alt="Me relaxing by some trees."/>
+        <CardMedia overlay={<Overlay />}>
+          <img src="http://colinvinson.com/images/me.jpg" alt="Me relaxing by some trees." width="200"/>
         </CardMedia>
       </Card>
       <Card className="QuickInfoCard Education">
